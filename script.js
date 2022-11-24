053ab895d5bb0a03d3b1cbe2f4b0ad20da7223d2
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2022-11-24 20:59:48 +0100

    js is killing me

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const hamburgerContainer = document.querySelector("#header__main-nav");
 const hamburger = document.querySelector(".header__main-nav--hamburger");
 const links = document.querySelectorAll("ul.header__main-nav--links li");
+const block = document.querySelectorAll("div.work__card");
+const text = document.querySelectorAll("div.work__card h3");
+
+console.log(text);
 
 hamburger.addEventListener("click", () => {
   hamburgerContainer.classList.toggle("clicked");
