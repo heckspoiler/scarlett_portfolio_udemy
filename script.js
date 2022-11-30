795f6573795415396fed9140036f30e6c0e2d386
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2022-11-30 11:59:24 +0100

    bö

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const hamburger = document.querySelector(".header__main-nav--hamburger");
 const links = document.querySelectorAll("ul.header__main-nav--links li");
 const block = document.querySelectorAll("div.work__card");
 const text = document.querySelectorAll("div.work__card h3");
-const workSection = document.querySelectorAll("#work");
+const workSection = document.getElementById("#work");
 
 console.log(text);
 
